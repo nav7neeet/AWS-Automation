e633cf36a0c84c636c4ec030f2935703fdbbaae0
Navneet <nav7neeet@gmail.com>
2022-11-17 19:22:49 +0530

    variable names updated

--- cross-accnt-role-audit.py
+++ cross-accnt-role-audit.py
@@ -3,22 +3,21 @@ import boto3
 from botocore.exceptions import ClientError
 import pandas
 
-REGION = "us-east-1"
-MANAGEMENT_X_ROLE = "list-accounts-role"
-READ_ONLY_X_ROLE = "read-only-role"
-MANAGEMENT_ACCOUNT_ID = "975300453774"
+MNGMT_ACCNT_ID = "000000000000"
+MNGMT_ACCNT_ROLE = "list-accounts-role"
+MEMBER_ACCNT_ROLE = "read-only-role"
 ROLE_SESSION_NAME = "cross-account-role-audit"
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 
-def build_X_role_arn(account_id, role_name):
-    role_arn = f"arn:aws:iam::{account_id}:role/{role_name}"
+def get_role_arn(accnt_id, role_name):
+    role_arn = f"arn:aws:iam::{accnt_id}:role/{role_name}"
     return role_arn
 
 
-def create_client(role_arn, service_name):
+def get_client(role_arn, service_name):
     sts_client = boto3.client("sts")
     response = sts_client.assume_role(
         RoleArn=role_arn, RoleSessionName=ROLE_SESSION_NAME
@@ -27,7 +26,6 @@ def create_client(role_arn, service_name):
 
     client = boto3.client(
         service_name,
-        region_name=REGION,
         aws_access_key_id=temp_creds["AccessKeyId"],
         aws_secret_access_key=temp_creds["SecretAccessKey"],
         aws_session_token=temp_creds["SessionToken"],
@@ -35,7 +33,7 @@ def create_client(role_arn, service_name):
     return client
 
 
-def create_resource(role_arn, service_name):
+def get_resource(role_arn, service_name):
     client = boto3.client("sts")
     response = client.assume_role(RoleArn=role_arn, RoleSessionName=ROLE_SESSION_NAME)
     temp_creds = response["Credentials"]
@@ -49,16 +47,16 @@ def create_resource(role_arn, service_name):
     return resource
 
 
-def list_aws_accounts(organizations):
-    accounts_list = []
+def get_accnt_list(organizations):
+    accnt_list = []
     paginator = organizations.get_paginator("list_accounts")
-    pages = paginator.paginate()
+    response_iterator = paginator.paginate()
 
-    for page in pages:
-        for account in page["Accounts"]:
-            accounts_list.append({"name": account["Name"], "id": account["Id"]})
+    for response in response_iterator:
+        for accnt in response["Accounts"]:
+            accnt_list.append({"name": accnt["Name"], "id": accnt["Id"]})
 
-    return accounts_list
+    return accnt_list
 
 
 def get_roles_list(iam):
@@ -66,20 +64,6 @@ def get_roles_list(iam):
     for role in iam.roles.all():
         roles_list.append(role)
     return roles_list
-
-
-def create_data_frame():
-    table = []
-    columns = [
-        "Account ID",
-        "Account Name",
-        "Role Name",
-        "Policy",
-        "Trust Relationship",
-        "X Access",
-    ]
-    data_frame = pandas.DataFrame(table, columns=columns)
-    return data_frame
 
 
 def get_role_details(role):
@@ -113,15 +97,29 @@ def get_role_details(role):
         return role_details
 
 
-def create_table(data_frame, role_details, account_id, account_name):
+def get_data_frame():
+    table = []
+    columns = [
+        "Account ID",
+        "Account Name",
+        "Role Name",
+        "Policy",
+        "Trust Relationship",
+        "X Access",
+    ]
+    data_frame = pandas.DataFrame(table, columns=columns)
+    return data_frame
+
+
+def create_table(data_frame, role_details, accnt_id, accnt_name):
     data_frame = pandas.concat(
         [
             data_frame,
             pandas.DataFrame.from_records(
                 [
                     {
-                        "Account ID": account_id,
-                        "Account Name": account_name,
+                        "Account ID": accnt_id,
+                        "Account Name": accnt_name,
                         "Role Name": role_details["name"],
                         "Policy": role_details["policy_names"],
                         "Trust Relationship": role_details["trust_relationship"],
@@ -135,29 +133,29 @@ def create_table(data_frame, role_details, account_id, account_name):
 
 
 def write_to_excel(table):
-    file_name = "cross-account-role-audit.xlsx"
+    file_name = "cross-accnt-role-audit.xlsx"
     table.to_excel(file_name)
 
 
 def main():
     try:
-        role_arn = build_X_role_arn(MANAGEMENT_ACCOUNT_ID, MANAGEMENT_X_ROLE)
+        role_arn = get_role_arn(MNGMT_ACCNT_ID, MNGMT_ACCNT_ROLE)
         # logger.info(role_arn)
-        client = create_client(role_arn, "organizations")
-        accounts_list = list_aws_accounts(client)
-        # logger.info(accounts_list)
-        data_frame = create_data_frame()
+        client = get_client(role_arn, "organizations")
+        accnt_list = get_accnt_list(client)
+        # logger.info(accnt_list)
+        data_frame = get_data_frame()
 
-        for account in accounts_list:
-            role_arn = build_X_role_arn(account["id"], READ_ONLY_X_ROLE)
+        for accnt in accnt_list:
+            role_arn = get_role_arn(accnt["id"], MEMBER_ACCNT_ROLE)
             try:
-                resource = create_resource(role_arn, "iam")
+                resource = get_resource(role_arn, "iam")
                 roles_list = get_roles_list(resource)
                 for role in roles_list:
                     role_details = get_role_details(role)
                     if role_details:
                         data_frame = create_table(
-                            data_frame, role_details, account["id"], account["name"]
+                            data_frame, role_details, accnt["id"], accnt["name"]
                         )
             except Exception as error:
                 logger.error(f"Failed to assume role: {role_arn} " + str(error))
